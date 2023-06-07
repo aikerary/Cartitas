9f47f73e2b53dbac9a884f1d001e8617db1174bf
Aiker Ary Acosta Cantillo <aikiringo26@gmail.com>
2023-06-07 16:01:46 -0500

    Starting project

---
+++ src/Header.jsx
@@ -0,0 +1,23 @@
+// import React from 'react' and useEffect and useState and Fragment;
+import React, { useEffect, useState, Fragment } from 'react';
+import logo from './logo.svg';
+import './css/Header.css';
+
+function Header() {
+    return (
+        <header className="App-header">
+            <a href="#">
+                <figure>
+                    <img src={logo} className="App-logo" alt="logo" />
+                </figure>
+            </a>
+            <ul>
+                <li><a href="#">How to play</a></li>
+                <li><a href="#">About me</a></li>
+                <li><a href="#">Repo</a></li>
+            </ul>
+        </header>
+    );
+}
+
+export default Header;
