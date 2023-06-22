90a069f6a6e9ef273f54e53ca4ec2b7e48cef84d
Aiker Ary Acosta Cantillo <aikiringo26@gmail.com>
2023-06-22 15:10:06 -0500

    Adding switch

--- src/Switch.jsx
+++ src/Switch.jsx
@@ -1,41 +1,53 @@
-import React, { useEffect, useState, Fragment } from 'react';
+import React, { useState, useEffect } from "react";
+import "./css/Switch.css"; // Asegúrate de tener el archivo CSS correspondiente
 
-const Switch = ({ onClick, night }) => {
+const Switch = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    document.body.classList.toggle("dark", isDarkMode);
+  }, [isDarkMode]);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
   return (
-    <div
-      className={`switch-wrapper ${!night ? "day" : "night"}`}
-      tabIndex={-1}
-      role="button"
-      onClick={onClick}
-    >
-      <div className={`sun-moon-combo ${!night ? "sun" : "moon"}`}>
-        <div className="moon-elem"></div>
-        <div className="moon-elem"></div>
-        <div className="moon-elem"></div>
-      </div>
-      <div className={`raybase ${night ? "hide" : "show"}`}>
-        <div className="ray-1"></div>
-        <div className="ray-2"></div>
-        <div className="ray-3"></div>
-      </div>
-      <div className={`raybase-2 ${!night ? "hide" : "show"}`}>
-        <div className="ray-1"></div>
-        <div className="ray-2"></div>
-        <div className="ray-3"></div>
-      </div>
-      {night && (
-        <div className="stars">
-          <div className="star"></div>
-          <div className="star"></div>
-          <div className="star"></div>
-          {[...Array(15)].map(() => {
-            return <div className="round-star"></div>;
-          })}
-        </div>
-      )}
+    <div className={`switch ${isDarkMode ? "dark" : ""}`}>
+      <input type="checkbox" onChange={toggleDarkMode} />
+      <span className="cursor">
+        <span className="moon-crater moon-crater-1"></span>
+        <span className="moon-crater moon-crater-2"></span>
+        <span className="moon-crater moon-crater-3"></span>
+      </span>
+      <span className="clouds">
+        <span className="cloud-particle cloud-particle-1"></span>
+        <span className="cloud-particle cloud-particle-2"></span>
+        <span className="cloud-particle cloud-particle-3"></span>
+        <span className="cloud-particle cloud-particle-4"></span>
+        <span className="cloud-particle cloud-particle-5"></span>
+        <span className="cloud-particle cloud-particle-6"></span>
+        <span className="cloud-particle cloud-particle-7"></span>
+      </span>
+      <span className="clouds-shadow">
+        <span className="cloud-particle cloud-particle-1"></span>
+        <span className="cloud-particle cloud-particle-2"></span>
+        <span className="cloud-particle cloud-particle-3"></span>
+        <span className="cloud-particle cloud-particle-4"></span>
+        <span className="cloud-particle cloud-particle-5"></span>
+        <span className="cloud-particle cloud-particle-6"></span>
+        <span className="cloud-particle cloud-particle-7"></span>
+      </span>
 
-      <div className="clouds-line-1" />
-      <div className="clouds-line-2" />
+      <span className="stars">
+        <span className="star star-1">&#9830;</span>
+        <span className="star star-2">&#9830;</span>
+        <span className="star star-3">&#9830;</span>
+        <span className="star star-4">&#9830;</span>
+        <span className="star star-5">&#9830;</span>
+        <span className="star star-6">&#9830;</span>
+        <span className="star star-7">&#9830;</span>
+      </span>
+      <span className="background"></span>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Header from './Header';
+import Switch from './Switch';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
+    <Switch />
   </React.StrictMode>
 );
 
