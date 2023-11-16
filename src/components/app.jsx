5630b30bfd91968624187557d7711492d2d91d52
Aiker Ary Acosta Cantillo <aikiringo26@gmail.com>
2023-11-15 19:07:07 -0500

    Card element working well

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,16 @@
 // Import the Header from header.jsx
 import { Header } from './header.jsx';
+import { Card } from './card.jsx';
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <p>Welcome to my app!</p>
+        {/* Use the card component with id 1 */}
+        <Card id={1} />
+        {/* Use the card component with id 2 */}
+        <Card id={2} />
       </main>
     </div>
   );
---
+++ src/components/card.jsx
@@ -0,0 +1,30 @@
+import { h, Component } from "preact";
+import "../css/card.css";
+
+export function Card({ id }) {
+    // Create a list of icons
+    const icons = ["cross", "star", "wave", "square"];
+    // Create a function that takes as an argument the actual icon
+    // and return a new icon not randomly, just moving to the next one
+    const nextIcon = (icon) => {
+        const index = icons.indexOf(icon);
+        return icons[(index + 1) % icons.length];
+    };
+
+    // When you click on card rotate it 360 degrees all the card
+    const handleClick = () => {
+        const card = document.querySelector(`#card-${id}`);
+        card.classList.toggle("rotate");
+        // Change the id of the span
+        const icon = card.querySelector(".icon");
+        icon.id = nextIcon(icon.id);
+    };
+
+    return (
+        <div id={`card-${id}`} className="card" onClick={handleClick}>
+            <span className="icon" id="cross"></span>
+        </div>
+    );
+}
+
+export default Card;
