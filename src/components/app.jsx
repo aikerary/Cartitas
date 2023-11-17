6dbeb6b31b34e93ee52f353c99f43fa5b8612d32
Aiker Ary Acosta Cantillo <aikiringo26@gmail.com>
2023-11-17 10:30:45 -0500

    Hand element finished

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,16 +1,13 @@
 // Import the Header from header.jsx
 import { Header } from './header.jsx';
-import { Card } from './card.jsx';
+import { Hand } from './hand.jsx';
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        {/* Use the card component with id 1 */}
-        <Card id={1} />
-        {/* Use the card component with id 2 */}
-        <Card id={2} />
+        <Hand />
       </main>
     </div>
   );
---
+++ src/components/hand.jsx
@@ -0,0 +1,17 @@
+import { h, Component } from "preact";
+import {Card} from "./card.jsx";
+import "../css/hand.css";
+
+export function Hand() {
+    return (
+        <div class="hand">
+            <Card id={1} />
+            <Card id={2} />
+            <Card id={3} />
+            <Card id={4} />
+            <Card id={5} />
+        </div>
+    );
+}
+
+export default Hand;
