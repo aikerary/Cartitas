497c9e87a98af6a93818302544836ccd7352c5fd
Aiker Ary Acosta Cantillo <aikiringo26@gmail.com>
2023-11-24 09:14:15 -0500

    All symbols working well

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,6 +2,7 @@
 import { Header } from './header.jsx';
 import { Hand } from './hand.jsx';
 import { Button } from './button.jsx';
+import Symbol from './symbol.jsx';
 // Import css from
 import "../css/app.css";
 import "../css/warning.css"
@@ -14,6 +15,12 @@ function App() {
         <Hand />
         <Button />
         <h2 className="warning"></h2>
+        {/* Create symbol with prop symbol=question */}
+        <Symbol symbol="question" />
+        <Symbol symbol="star" />
+        <Symbol symbol="cross" />
+        <Symbol symbol="wave" />
+        <Symbol symbol="square" />
       </main>
     </div>
   );
