5cb556ad6c1be7890b99c5601a69b0e4bf254e67
Aiker Ary Acosta Cantillo <aikiringo26@gmail.com>
2023-12-19 12:45:55 -0500

    Logic of restart working well

--- src/components/restart.jsx
+++ src/components/restart.jsx
@@ -1,9 +1,19 @@
-import { h } from "preact";
+import { h, render } from "preact";
+import { Game } from "./game";
 import "../css/restart.css";
 
 export function Restart() {
   const handleClick = () => {
-    window.location.reload();
+    // Get the main
+    const main = document.querySelector("main");
+    // Void the main
+    main.innerHTML = "";
+    // Create a fragment
+    const fragment = document.createDocumentFragment();
+    // Render the game in the fragment
+    render(<Game />, fragment);
+    // Append to the main
+    main.appendChild(fragment);
   };
   return (
     <button class="btn restart" onClick={handleClick}>
