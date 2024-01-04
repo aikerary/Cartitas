9ba7dcad0d8c14ef2e26d9bf1766a67e7f0ffb2d
Aiker Ary Acosta Cantillo <aikiringo26@gmail.com>
2024-01-04 12:35:47 -0500

    First design of footer ended

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,6 +4,20 @@ import "../css/footer.css";
 export function Footer(){
     return (
         <footer className="footer">
+            <div className="advice">
+                This page was created by A.A.Ary
+            </div>
+            <div className="social">
+                <a href="https://www.github.com/aikerary" className="social-icon" target="_blank" rel="noopener noreferrer">
+                    <span className="github"></span>
+                </a>
+                <a href="https://www.linkedin.com/in/aiker-acosta-cantillo-03422422b" className="social-icon" target="_blank" rel="noopener noreferrer">
+                    <span className="linkedin"></span>
+                </a>
+                <a href="https://aiker-cev.pages.dev/" className="social-icon" target="_blank" rel="noopener noreferrer">
+                    <span className="personal"></span>
+                </a>
+            </div>
         </footer>
     );
 }
